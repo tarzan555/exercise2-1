e2f19302b09e0b556501e135cd13871d6b99275d
Max Nigsch <tarzan555@gmx.de>
2013-05-15 18:32:52 +0200

    principal game logic implemented

--- src/com/example/muc13_02_bachnigsch/GameActivity.java
+++ src/com/example/muc13_02_bachnigsch/GameActivity.java
@@ -18,17 +18,129 @@ import android.widget.TextView;
 public class GameActivity extends Activity {
 
 	GestureHandler gestureHandler = new GestureHandler(this);
-	
+	TextView textView2;
+	TextView textView3;
+	String gestureName;
+	String receivedGestureName;
 	
 	public void gestureTest (View view){
-		
-		System.out.println("Geste: " + gestureHandler.getGestureName());
-		
-		TextView textView;
-		textView = (TextView)findViewById(R.id.textView2);
-		textView.setText("Geste: " + gestureHandler.getGestureName());
+		textView2 = (TextView)findViewById(R.id.textView2);
+		textView2.setText("Geste: " + gestureHandler.getGestureName());
 	}
 
+	
+	
+	/**
+	 * Game Logic
+	 */
+	
+	
+	int points;
+	int player;
+	
+	
+	public void startGame(View view){
+		
+		
+		// TODO: get to know wether your are server or client
+		
+		// initaial situation: server is player 1, client is player 2
+		player = 1; // or 2
+		
+		
+		// play loop
+		for (int i = 0 ; i < 11 ; i++){
+		
+			if(player == 1){
+				
+				/**
+				 * It's player 1 's turn
+				 */				
+				
+				// player has to perform a gesture
+				textView2 = (TextView)findViewById(R.id.textView2);
+				textView2.setText("Du bist dran - mache eine Geste!");
+				
+				// get performed gesture from Gesture Handler
+				gestureName = gestureHandler.getGestureName();
+				
+				// TODO: send performed gesture name via bluetooth
+							
+				// TODO: recieve repeated gesture name via bluetooth
+				
+				receivedGestureName = "example";
+				
+				// compare receied with performed gesture
+				
+				// player 1 looses
+				if (receivedGestureName.equals(gestureName)){
+					// in next loop you are player 2
+					player = 2;			
+				}
+				
+				// player 1 wins
+				else {
+					points ++;
+					// in next loop you are player 1
+					player = 1;
+				}
+				
+				textView3 = (TextView)findViewById(R.id.textView3);
+				textView3.setText("Du hast " + points + " Punkte");
+				
+			}
+			
+			
+			if(player == 2){
+				
+				/**
+				 * It's player 2's turn
+				 */
+			
+				textView2 = (TextView)findViewById(R.id.textView2);
+				textView2.setText("Warte auf Geste von Gegner");
+				
+				// TODO: receive gesture via bluetooth
+				// wait for gesture via bluetooth from other player
+				receivedGestureName = "example";
+				
+				textView2.setText("Geste empfangen - wiederhole sie!");
+	
+				// get performed gesture from gesture handler
+				gestureName = gestureHandler.getGestureName();
+				
+				
+				// gestures are according - player 2 wins
+				if (receivedGestureName.equals(gestureName)){
+					points ++;
+					// in next loop you are player 1
+					player = 1;
+					
+					
+				}
+				
+				// gestures are different - player 2 looses
+				else {
+					// in next loop you are player 2
+					player = 2;
+				}
+				
+				textView3 = (TextView)findViewById(R.id.textView3);
+				textView3.setText("Du hast " + points + " Punkte");
+			}
+			
+			
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	/**
 	 * Activity Stuff
