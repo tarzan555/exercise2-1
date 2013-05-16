3846d720c47bd6b193aea48448065f4052c2365f
Max Nigsch <tarzan555@gmx.de>
2013-05-16 21:49:20 +0200

    lalalala sack

--- src/com/example/muc13_02_bachnigsch/GameActivity.java
+++ src/com/example/muc13_02_bachnigsch/GameActivity.java
@@ -28,7 +28,7 @@ public class GameActivity extends Activity {
 	public void gestureTest (View view){
 		textView2 = (TextView)findViewById(R.id.textView2);
 		TreeMap mTreeMap = gestureHandler.getGesture();
-		mTreeMap.subMap(fromInclusive, toExclusive); //bla
+		mTreeMap.subMap(fromInclusive, toExclusive); //bla hallo penis
 		textView2.setText("Geste: " + gestureHandler.getGesture().);
 	}
 
